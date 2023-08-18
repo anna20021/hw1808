b2be7f65e12fdec6e51ce54762b5f70adf51b04b
anna20021 <pavalaki.snezhana@mail.ru>
2023-08-18 13:08:01 +1000

    solved ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного 
+//числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine ("number");
+int number = int.Parse(Console.ReadLine());
+int a = 0;
+if (number < 100)
+{
+    Console.WriteLine(" число не имеет 3 цифры");
+}
+else 
+{
+a = number % 10;
+Console.WriteLine(a);
+}
