c0b7f110d9e67c1752c93d205cf45d124960be52
anna20021 <pavalaki.snezhana@mail.ru>
2023-08-18 13:15:27 +1000

    solved ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.WriteLine ("number from 1 to 7");
+int number = int.Parse(Console.ReadLine());
+if ( number <= 5)
+{
+    Console.WriteLine("work day");
+}
+else 
+{ 
+    Console.WriteLine("day off");
+}
